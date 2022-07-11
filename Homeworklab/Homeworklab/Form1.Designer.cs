da8e5c7bb937b6b4e73d74f370fa82adb8f01852
Lenglee-vaja <104408080+Lenglee-vaja@users.noreply.github.com>
2022-07-11 11:20:15 +0700

    Add files via upload

---
+++ Homeworklab/Homeworklab/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Homeworklab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbnum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbname = new System.Windows.Forms.ComboBox();
+            this.cbclass = new System.Windows.Forms.ComboBox();
+            this.cbgender = new System.Windows.Forms.ComboBox();
+            this.cbyear = new System.Windows.Forms.ComboBox();
+            this.tbmon = new System.Windows.Forms.TextBox();
+            this.cbid = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgfees = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgfees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Phetsarath OT", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(588, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ໃບຈ່າຍເງີນ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ເລກທີ່";
+            // 
+            // tbnum
+            // 
+            this.tbnum.Location = new System.Drawing.Point(140, 51);
+            this.tbnum.Name = "tbnum";
+            this.tbnum.Size = new System.Drawing.Size(288, 49);
+            this.tbnum.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 41);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ຊື່";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 41);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ຫ້ອງ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 385);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 41);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ເພດ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-1, 537);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 41);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ຈຳນວນເງີນ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 461);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 41);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "ປີຮຽນ";
+            // 
+            // cbname
+            // 
+            this.cbname.FormattingEnabled = true;
+            this.cbname.Location = new System.Drawing.Point(136, 198);
+            this.cbname.Name = "cbname";
+            this.cbname.Size = new System.Drawing.Size(288, 49);
+            this.cbname.TabIndex = 8;
+            // 
+            // cbclass
+            // 
+            this.cbclass.FormattingEnabled = true;
+            this.cbclass.Location = new System.Drawing.Point(136, 289);
+            this.cbclass.Name = "cbclass";
+            this.cbclass.Size = new System.Drawing.Size(288, 49);
+            this.cbclass.TabIndex = 9;
+            // 
+            // cbgender
+            // 
+            this.cbgender.FormattingEnabled = true;
+            this.cbgender.Items.AddRange(new object[] {
+            "female",
+            "male"});
+            this.cbgender.Location = new System.Drawing.Point(136, 382);
+            this.cbgender.Name = "cbgender";
+            this.cbgender.Size = new System.Drawing.Size(288, 49);
+            this.cbgender.TabIndex = 10;
+            // 
+            // cbyear
+            // 
+            this.cbyear.FormattingEnabled = true;
+            this.cbyear.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.cbyear.Location = new System.Drawing.Point(136, 453);
+            this.cbyear.Name = "cbyear";
+            this.cbyear.Size = new System.Drawing.Size(288, 49);
+            this.cbyear.TabIndex = 11;
+            // 
+            // tbmon
+            // 
+            this.tbmon.Location = new System.Drawing.Point(136, 534);
+            this.tbmon.Name = "tbmon";
+            this.tbmon.Size = new System.Drawing.Size(288, 49);
+            this.tbmon.TabIndex = 12;
+            // 
+            // cbid
+            // 
+            this.cbid.FormattingEnabled = true;
+            this.cbid.Location = new System.Drawing.Point(140, 124);
+            this.cbid.Name = "cbid";
+            this.cbid.Size = new System.Drawing.Size(288, 49);
+            this.cbid.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(38, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 41);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "ລະຫັດ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(401, 602);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 46);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "ບັນທືກ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgfees
+            // 
+            this.dgfees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgfees.Location = new System.Drawing.Point(487, 59);
+            this.dgfees.Name = "dgfees";
+            this.dgfees.RowHeadersWidth = 62;
+            this.dgfees.RowTemplate.Height = 28;
+            this.dgfees.Size = new System.Drawing.Size(665, 524);
+            this.dgfees.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1366, 690);
+            this.Controls.Add(this.dgfees);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbid);
+            this.Controls.Add(this.tbmon);
+            this.Controls.Add(this.cbyear);
+            this.Controls.Add(this.cbgender);
+            this.Controls.Add(this.cbclass);
+            this.Controls.Add(this.cbname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbnum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Phetsarath OT_Italic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgfees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbnum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbname;
+        private System.Windows.Forms.ComboBox cbclass;
+        private System.Windows.Forms.ComboBox cbgender;
+        private System.Windows.Forms.ComboBox cbyear;
+        private System.Windows.Forms.TextBox tbmon;
+        private System.Windows.Forms.ComboBox cbid;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgfees;
+    }
+}
+
---
+++ Homeworklab/Homeworklab/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Homeworklab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ACER\Documents\LabHw.mdf;Integrated Security=True;Connect Timeout=30");
+        private void fillstudentid()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select stid From tbstudent", con);
+            SqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("stid", typeof(string));
+            dt.Load(rdr);
+            cbid.ValueMember = "stid";
+            cbid.DataSource = dt;
+            con.Close();
+        }
+        private void fillstudentname()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select stname From tbstudent", con);
+            SqlDataReader rd;
+            rd = cmd.ExecuteReader();
+            DataTable dt2 = new DataTable();
+            dt2.Columns.Add("stname", typeof(string));
+            dt2.Load(rd);
+            cbname.ValueMember = "stname";
+            cbname.DataSource = dt2;
+            con.Close();
+        }
+        private void fillstudentclass()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select stclass From tbstudent", con);
+            SqlDataReader rd;
+            rd = cmd.ExecuteReader();
+            DataTable dt3 = new DataTable();
+            dt3.Columns.Add("stclass", typeof(string));
+            dt3.Load(rd);
+            cbclass.ValueMember = "stclass";
+            cbclass.DataSource = dt3;
+            con.Close();
+        }
+        private void populate()
+        {
+            con.Open();
+            string query = "select * from tbfees";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            dgfees.DataSource = ds.Tables[0];
+            con.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillstudentid();
+            fillstudentname();
+            fillstudentclass();
+            populate();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /*tbnum.Text = dgfees.SelectedRows[0].Cells[0].Value.ToString();
+            cbgender.SelectedItem = dgfees.SelectedRows[0].Cells[4].Value.ToString();
+            tbmon.Text = dgfees.SelectedRows[0].Cells[6].Value.ToString();
+            
+            StdPhone.Text = StdDGV.SelectedRows[0].Cells[4].Value.ToString();
+            FeesTb.Text = StdDGV.SelectedRows[0].Cells[6].Value.ToString();*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             try
+             {
+                 if (tbnum.Text == "" || tbmon.Text == "" )
+                 {
+                     MessageBox.Show("Missing Information");
+                 }
+                 else
+                 {
+                     con.Open();
+                     SqlCommand cmd = new SqlCommand("insert into tbfees values('" + tbnum.Text + "','" + cbid.SelectedValue.ToString() + "','" + cbname.SelectedValue.ToString() + "','" + cbclass.SelectedValue.ToString() + "','" + cbgender.SelectedItem.ToString() + "','" + cbyear.SelectedItem.ToString() + "','" + tbmon.Text + "')", con);
+                     cmd.ExecuteNonQuery();
+                     MessageBox.Show("Fees Successfully Added");
+                     con.Close();
+                     populate();
+                 }
+             }
+             catch
+             {
+                 MessageBox.Show("Something Went Wrong");
+             }
+            /*dgfees.Rows.Add(tbnum.Text, cbid.SelectedValue.ToString(), cbname.SelectedValue.ToString(), cbclass.SelectedValue.ToString(), cbgender.SelectedItem.ToString(), cbyear.SelectedItem.ToString(), tbmon.Text);*/
+        }
+    }
+}
